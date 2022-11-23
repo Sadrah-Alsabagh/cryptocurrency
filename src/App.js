335e1894362e2a5e0e45af40acf6da4bcd0cbbee
Sadrah-Alsabagh <sedrasabbagh33@gmail.com>
2022-11-23 13:34:51 +0000

    make the basic layout

--- src/App.js
+++ src/App.js
@@ -36,8 +36,16 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
+      
+      <div className='footer' >
+<Typography.Title level={5} style={{color:'white',textAlign:'center'}}>Crypto<br/>
+All rights reserved &copy; sedra {new Date().getFullYear()}</Typography.Title>
+<Space><Link to='/'>Home</Link>
+<Link to='/exchanges'>Exchanges</Link>
+<Link to='/news'>News</Link>
+</Space>
+</div>
       </div>
-      <div className='footer'></div>
     </div>
   )
 }
