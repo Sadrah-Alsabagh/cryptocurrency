6219c11ce9d0188c128406f58d926831d29a7f6c
Sadrah-Alsabagh <sedrasabbagh33@gmail.com>
2022-11-23 19:13:53 +0000

    almost finish the project

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
-import {Layout, Typography, Space} from 'antd';
-import {Navbar, Exchanges, HomePage, CryptoCurrencies, News, CryptoDetails} from './components';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Exchanges, HomePage, News, CryptoCurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
-const App = () => {
-  return (
-    <div className='app'>
-      <div className='navbar'>
-        <Navbar />
-      </div>
-      <div className='main'>
-        <Layout>
-          <div className='routes'>
-            <Switch>
-              <Route exact path ="/">
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
               <HomePage />
-              </Route>
-
-              <Route exact path ="/exchanges">
+            </Route>
+            <Route exact path="/exchanges">
               <Exchanges />
-              </Route>
-
-              <Route exact path ="/cryptocurrencies">
+            </Route>
+            <Route exact path="/CryptoCurrencies">
               <CryptoCurrencies />
-              </Route>
-
-              <Route exact path ="/crypto/:coinId">
+            </Route>
+            <Route exact path="/crypto/:coinId">
               <CryptoDetails />
-              </Route>
-
-              <Route exact path ="/news">
+            </Route>
+            <Route exact path="/news">
               <News />
-              </Route>
-            </Switch>
-          </div>
-        </Layout>
-      
-      <div className='footer' >
-<Typography.Title level={5} style={{color:'white',textAlign:'center'}}>Crypto<br/>
-All rights reserved &copy; sedra {new Date().getFullYear()}</Typography.Title>
-<Space><Link to='/'>Home</Link>
-<Link to='/exchanges'>Exchanges</Link>
-<Link to='/news'>News</Link>
-</Space>
-</div>
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
       </div>
     </div>
-  )
-}
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Provider} from 'react-redux';
 import store from './app/store';
 
-ReactDOM.render(
-    <Router>
-        <Provider store ={store}>
-<App />
-</Provider>
-</Router>,
-document.getElementById('root'));
 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
